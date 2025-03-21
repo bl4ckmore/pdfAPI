62e466f372c7a81a989f4861c7889ab40813c8a1
bl4ckmore <blackmorevar@gmail.com>
2025-03-22 02:38:08 +0400

    now works with postgres ;)

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 // 📁 db.js
+require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: "postgresql://master_4lcu_user:azhrIZSnUReYz3HSbjztKTy1sMtsiNnu@dpg-cvetc59opnds73ein3v0-a.oregon-postgres.render.com/master_4lcu",
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
